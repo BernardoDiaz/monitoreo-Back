e20fea3fa8387880ff2e831bf065a95ba0b5859d
BernardoDiaz <bernardo17.bd@gmail.com>
2025-06-23 20:28:29 -0600

    update2603-db

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('seguimiento_clientes2', 'root', 'root123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-// const sequelize = new Sequelize('seguimiento_clientes2','bernardo','test123',{
+// const sequelize = new Sequelize('seguimiento_clientes2','root','root123456',{
 //     host: 'localhost',
 //     dialect: 'mysql'
 // }); 
+const sequelize = new sequelize_1.Sequelize('seguimiento_clientes2', 'bernardo', 'test123', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 exports.default = sequelize;
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,14 +1,14 @@
 import {Sequelize} from "sequelize";
 
 
-const sequelize = new Sequelize('seguimiento_clientes2','root','root123456',{
-    host: 'localhost',
-    dialect: 'mysql'
-}); 
-
-// const sequelize = new Sequelize('seguimiento_clientes2','bernardo','test123',{
+// const sequelize = new Sequelize('seguimiento_clientes2','root','root123456',{
 //     host: 'localhost',
 //     dialect: 'mysql'
 // }); 
+
+const sequelize = new Sequelize('seguimiento_clientes2','bernardo','test123',{
+    host: 'localhost',
+    dialect: 'mysql'
+}); 
 
 export default sequelize;
